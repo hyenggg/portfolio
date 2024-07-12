96135074ca693e9960e4f12dbfedc0d89854f3c5
hyenggg <hwyoon022@naver.com>
2024-07-12 15:21:31 +0900

    포트폴리오(수정07.12)

--- js/modal2.js
+++ js/modal2.js
@@ -20,4 +20,14 @@ $(document).ready(function(){
     $("html").css({"overflow-y":"scroll"}); 
   });
 
+  $(".go3 a:nth-child(2)").click(function(){
+    $("html").css({"overflow-y":"hidden"});
+    $(".modal5").fadeIn();
+  });
+
+  $(".btn_close3").click(function(){
+    $(".modal5").fadeOut();
+    $("html").css({"overflow-y":"scroll"}); 
+  });
+
 });
